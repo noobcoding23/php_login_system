8bd61f795110ba75c9055b94b086eede6ea23e69
noobcoding23 <jugalkdas99@gmail.com>
2020-12-24 18:33:42 +0530

    sign up added

---
+++ signup.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/CSS/master.css">
+
+    <title>SignUp</title>
+  </head>
+  <body>
+
+  <?php require 'partials/_nav.php' ?>
+
+  <div class="container my-4">
+  <h1 class="text-center">Signup to our website</h1>
+
+  <form action="/signup.php" method="post">
+  <div class="mb-3  my-4">
+    <label for="username" class="form-label">Username</label>
+    <input type="email" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3">
+    <label for="cpassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+    <div id="emailHelp" class="form-text">Make sure to type the same password</div>
+  </div>
+  <button type="submit" class="btn btn-primary">SignUp</button>
+</form>
+  </div>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
