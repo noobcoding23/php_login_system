73b2031c764f31552a75edc545dde7eedc91774c
noobcoding23 <jugalkdas99@gmail.com>
2020-12-24 21:33:50 +0530

    logged out finished

--- welcome.php
+++ welcome.php
@@ -7,24 +7,45 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 }
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+      crossorigin="anonymous"
+    />
 
     <title>Welcome - <?php echo $_SESSION['username']?></title>
   </head>
   <body>
+    <?php require 'partials/_nav.php' ?>
+    <div class="container">
+      <div class="alert alert-success my-4" role="alert">
+        <h4 class="alert-heading">Welcome -
+      <?php echo $_SESSION['username']?>!</h4>
+        <p>
+          Hey how are you doing. Welcome to iSecure. You are logged in as <strong>"<?php echo $_SESSION['username']?>"</strong>. Aww yeah, you successfully read this important alert message. This
+          example text is going to run a bit longer so that you can see how
+          spacing within an alert works with this kind of content.
+        </p>
+        <hr />
+        <p class="mb-0">
+          Whenever you need to, be sure to logout <a href="/logout.php">using this link.</a>
+        </p>
+      </div>
+    </div>
 
-  <?php require 'partials/_nav.php' ?>
-  <h1 class="text-center my-4">Welcome - <?php echo $_SESSION['username']?></h1>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
